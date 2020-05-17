ae73634f302dda694384450e7944d0dbda6ba2ed
igorgcosta <igorgabriel@outlook.com>
2020-05-16 21:32:55 -0300

    adicionando classes e testes

---
+++ codigo_avulso_test_tutorial/circulo.py
@@ -0,0 +1,15 @@
+import math
+from codigo_avulso_test_tutorial.figura_geometrica import FiguraGeometrica
+
+class Circulo(FiguraGeometrica):
+
+    def __init__(self):
+      self.raio = 0
+
+    # Retorna a area do circulo
+    def get_area(self):
+        return math.pi * self.raio**2
+
+    # Retorna o perimetro do circulo
+    def get_perimetro(self):
+        return 2 * math.pi * self.raio
---
+++ setup.py
@@ -0,0 +1,6 @@
+from setuptools import setup
+setup(
+  name='codigo-avulso-test-tutorial',
+  packages=['codigo_avulso_test_tutorial', 'test'],
+  test_suite='test',
+)
